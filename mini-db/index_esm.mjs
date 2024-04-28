26be33cb15f8f6a9955c12d8b457455a5a52d7f8
bogeeee <118847342+bogeeee@users.noreply.github.com>
2024-04-28 01:08:19 +0000

    Delete all files here, cause moved to a new repo.

--- mini-db/index_esm.mjs
+++
@@ -1,8 +0,0 @@
-// A wrapper file for ESM to avoid the 'dual package hazard'. See https://nodejs.org/api/packages.html#approach-1-use-an-es-module-wrapper
-
-
-import cjsIndex from "./index.js"
-export const MiniDb = cjsIndex.MiniDb
-
-import cjsDecorators from "./decorators.js"
-export const persistence = cjsDecorators.persistence
